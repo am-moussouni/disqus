f64065ebd6a5e2b624f969cf372d382e6c2226fe
imadimadimadimad <104800012+imadimadimadimad@users.noreply.github.com>
2023-04-16 17:17:45 +0200

    Add files via upload

---
+++ server/src/entities/friends.js
@@ -0,0 +1,14 @@
+class Friends {
+  constructor(db) {
+    this.db = db
+  }
+
+	// add friendship to database 
+	
+    // get friends of user from database, returns the record
+
+    // check friendship between 2 users, returns boolean
+
+	// delete friendship
+  
+exports.default = Friends;
